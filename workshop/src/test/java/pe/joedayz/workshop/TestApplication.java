645184feca2959f947644702dea670cfc2d771b2
Jose Diaz <jose.diaz@joedayz.pe>
2026-07-07 19:49:38 -0500

    template inicial

---
+++ workshop/src/test/java/pe/joedayz/workshop/TestApplication.java
@@ -0,0 +1,10 @@
+package pe.joedayz.workshop;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+    }
+}
